03a462199f122d7746acc19a01a17b68b8fa6fcf
Kohki_Takatama <132065581+Kohki-Takatama@users.noreply.github.com>
2023-05-16 18:50:08 +0900

    Update react.sh

--- react.sh
+++ react.sh
@@ -1,1 +1,17 @@
-$ npm
+# normal
+npx create-react-app appName
+cd appName
+npm start
+### open launch.json
+### rewrite "url" to localhost number
+
+# Vite (呼：ヴィート・ヴィーツ 構築が高速で実用的なテンプレート)
+npm create vite@latest
+Project name: projectName
+select a flamework: react
+select a variant: react
+cd projectName
+npm install
+npm run dev
+### open launch.json
+### rewrite "url" to localhost number
