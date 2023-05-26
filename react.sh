a9c4c276177bf898f8d608f354cde0ac813ba31e
Kohki_Takatama <132065581+Kohki-Takatama@users.noreply.github.com>
2023-05-26 11:44:19 +0900

    Update react.sh

--- react.sh
+++ react.sh
@@ -8,8 +8,8 @@ npm start
 # Vite (呼：ヴィート・ヴィーツ 構築が高速で実用的なテンプレート)
 npm create vite@latest
 Project name: projectName
-select a flamework: react
-select a variant: react
+select a flamework: React
+select a variant: JaveScript
 cd projectName
 npm install
 npm run dev
